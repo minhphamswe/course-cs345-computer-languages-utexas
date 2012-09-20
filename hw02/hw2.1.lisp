87855ee152ce931ba73c0ddddb6d0f690f0d6620
Minh Pham <minh.pham@utexas.edu>
2012-09-20 15:52:47 -0500

    Problem 1 for homework 2 done. The functions (copied from the book) seems to harbor redundancies.

--- hw02/hw2.1.lisp
+++ hw02/hw2.1.lisp
@@ -13,10 +13,25 @@
 ;;;; In the let statement for the solution show that 2^10 = 1024.
 ;;;; Note: By the way, you should see that f9 is multiplication.
 
-(let ((f1 (lambda (x) (+ 1 x)))
-      (f2 (lambda (x) x))
-      (f3 (lambda (x y z) y))
-      (f4 (lambda (x y z) (f1 (f3 x y z)))))
-  (letrec ((f5 (lambda (x y))
-               (if 
+(let ((f1 (lambda (x) (+ 1 x)))   ; add one (successor)
+      (f2 (lambda (x) x))         ; identity (= x + 0)
+      (f3 (lambda (x y z) y)))    ; 2nd identity
+  (let ((f4 (lambda (x y z) (f1 (f3 x y z)))))   ; y + 1 (successor of 2nd id)
+    (letrec ((f5 (lambda (x y) (if (= 0 x)
+                                   (f2 y)
+                                   (f4 (- x 1) (f5 (- x 1) y) y)))))  ; (+ x y)
+      (let ((f6 (lambda (x y z) z)))   ; 3rd identity
+        (let ((f7 (lambda (x y z) (f5 (f3 x y z) (f6 x y z))))   ; (+ y z)
+              (f8 (lambda (x) 0)))     ; 0 (= x * 0)
+          (letrec ((f9 (lambda (x y) (if (= 0 x)
+                                         (f8 x)
+                                         (f7 x (f9 (- x 1) y) y)))))   ; (* x y)
+            (let ((f10 (lambda (x y z) (f9 (f3 x y z) (f6 x y z))))    ; (* y z)
+                  (f11 (lambda (x) 1)))   ; 1 (= x^0)
+              (letrec ((f12 (lambda (x y) (if (= 0 x)
+                                              (f11 y)
+                                              (f10 x (f12 (- x 1) y) y)))))
+                ; f12 is y raised to the power of x
+                (f12 10 2)))))))))
+  
 
