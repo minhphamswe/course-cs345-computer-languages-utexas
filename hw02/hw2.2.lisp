3968caec123ad94da9ea54d38ba79f9f20b60280
Minh Pham <minh.pham@utexas.edu>
2012-09-20 16:54:05 -0500

    Working on homework 2 problem 2.

--- hw02/hw2.2.lisp
+++ hw02/hw2.2.lisp
@@ -11,3 +11,18 @@
 ;;;; manner similar to problem 1 above. In the let statement for the solutions
 ;;;; build a list that shows that 11 = 11, 11 != 12, and 12 != 11. i.e., the
 ;;;; let should return '(0 1 1).
+
+(letrec ((pred (lambda (x) (if (= x 0)
+                               0          ; only works for x > 0
+                               (- x 1)))) ; x-1 (predecessor fn)
+         ; hf-minus (half-minus) is 
+         (hf-minus (lambda (x y) (if (= y 0)
+                                     x
+                                     (pred (hf-minus (x (pred y)))))))
+         
+         
+                 
+         
+  
+              
+
